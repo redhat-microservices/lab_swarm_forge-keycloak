3aa69039e34e613d3d793174537cc2426c14a110
Sebastien Blanc <scm.blanc@gmail.com>
2016-09-07 11:15:27 +0200

    correct port

--- scripts/frontend_assets/sellingpoint/SellingPointFactory.js
+++ scripts/frontend_assets/sellingpoint/SellingPointFactory.js
@@ -1,4 +1,4 @@
 angular.module('bookservice').factory('SellingPointResource', function($resource){
-    var resource = $resource('http://localhost:8083/rest/sellingpoints/inrange/:Isbn',{Isbn:'@isbn'},{'queryAll':{method:'GET',isArray:true},'query':{method:'GET',isArray:false},'update':{method:'PUT'}});
+    var resource = $resource('http://localhost:8082/rest/sellingpoints/inrange/:Isbn',{Isbn:'@isbn'},{'queryAll':{method:'GET',isArray:true},'query':{method:'GET',isArray:false},'update':{method:'PUT'}});
     return resource;
 });
